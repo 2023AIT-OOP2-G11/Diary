6a6dbf6d2196083a8c9cbf12659135e48423c013
mi-1227 <140976341+mi-1227@users.noreply.github.com>
2023-12-14 23:11:02 +0900

    頭を大文字に変更

--- diaries/kobayashiDiary.py
+++ diaries/kobayashiDiary.py
@@ -1,6 +1,6 @@
 from diaries.AbstractDiary import AbstractDiary
 
-class kobayashiDiary(AbstractDiary):
+class KobayashiDiary(AbstractDiary):
     def get_date(self):
         return "2021-12-14"
 
--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from diaries.ToriiDiary import ToriiDiary
 from diaries.ItoDiary import ItoDiary
 from diaries.kageyamaDiary import kageyamaDiary
 from diaries.UsidaDiary import UsidaDiary
-from diaries.kobayashiDiary import kobayashiDiary
+from diaries.KobayashiDiary import KobayashiDiary
+
 # ↓のリストには、メンバーの各日記が格納されます。 diaries = [DiarySample(), ]
-diaries = [DiarySample(), 
-           YamamotoDiary(),
-           ToriiDiary(),
-           ItoDiary(),
-           kageyamaDiary(),
-           UsidaDiary(),
-           kobayashiDiary()
-           ]
+diaries = [
+    DiarySample(),
+    YamamotoDiary(),
+    ToriiDiary(),
+    ItoDiary(),
+    kageyamaDiary(),
+    UsidaDiary(),
+    KobayashiDiary(),
+]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
