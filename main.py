8952affc0188b8648387117de3ed7304b98b3b63
WaWaWa-www <mountainbook1015@gmail.com>
2023-12-14 15:43:19 +0900

    山本日記

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.YamamotoDiary import YamamotoDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。 diaries = [DiarySample(), ]
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), 
+           YamamotoDiary(),
+           ]
 
 for d in diaries:
     print("---------------------------------")
