e7ffa0cca93aa26843b416c836f3767225c531de
WaWaWa-www <mountainbook1015@gmail.com>
2023-12-14 15:53:18 +0900

    Update ToriiDiary.py

--- diaries/ToriiDiary.py
+++ diaries/ToriiDiary.py
@@ -5,5 +5,5 @@ class ToriiDiary(AbstractDiary):
 def get_summary(self):
     return """今日はオブジェクト指向プログラミング演習2のグループワーク演習だった。
 この資料を作成するのは、正直しんどかった。 資料に書かれてないことも勝手に伝われば良いのに。と思った。"""
-    def get_author(self):
-        return "Torii"
+def get_author(self):
+    return "Torii"
