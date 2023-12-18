8cd0aa3ef524e337577b4bf49231276b42d560e0
YUKA TORII <manjon1210@gmail.com>
2023-12-18 21:12:54 +0900

    コンフリクト解決

--- diaries/ToriiDiary.py
+++ diaries/ToriiDiary.py
@@ -3,7 +3,7 @@ from diaries.AbstractDiary import AbstractDiary
 class ToriiDiary(AbstractDiary):
     def get_date(self):
         return "2023-12-18"
-def get_summary(self):
-    return """github使いにくいです。"""
+    def get_summary(self):
+        return """github使いにくいです。"""
     def get_author(self):
         return "Torii"
