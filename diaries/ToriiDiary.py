2fc5c6f65e4593faf5da790cf5f9b44b206720cd
YUKA TORII <manjon1210@gmail.com>
2023-12-18 21:28:18 +0900

    Update ToriiDiary.py

--- diaries/ToriiDiary.py
+++ diaries/ToriiDiary.py
@@ -4,6 +4,6 @@ class ToriiDiary(AbstractDiary):
     def get_date(self):
         return "2023-12-18"
     def get_summary(self):
-        return """github使いにくいです。"""
+        return """github使いにくいです。間違っていないかどうか不安です。"""
     def get_author(self):
         return "Torii"
